aa59b5bb9c7dc2811258c9bb3890974ccf892f11
Lizzie <lizziejacoboo@gmail.com>
2024-02-19 18:39:25 -0700

    added routes files, html routes, corrected server.js

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
